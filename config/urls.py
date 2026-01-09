c8d9cf929280f6ac4b45bb9ee0c350887bc6e338
kravetst <taraskravetc28@gmail.com>
2026-01-09 11:17:10 +0200

    Add API viewsets and routes for notifications

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from rest_framework.routers import DefaultRouter
+from notifications.views import NotificationTypeViewSet, NotificationTemplateViewSet
+
+router = DefaultRouter()
+router.register("notification-types", NotificationTypeViewSet)
+router.register("notification-templates", NotificationTemplateViewSet)
+
+urlpatterns = router.urls
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import NotificationType, NotificationTemplate
+from .serializers import (
+    NotificationTypeSerializer,
+    NotificationTemplateSerializer,
+)
+from .permissions import IsAdminOrReadOnly
+
+
+class NotificationTypeViewSet(ModelViewSet):
+    queryset = NotificationType.objects.all()
+    serializer_class = NotificationTypeSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class NotificationTemplateViewSet(ModelViewSet):
+    queryset = NotificationTemplate.objects.select_related("type")
+    serializer_class = NotificationTemplateSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def perform_destroy(self, instance):
+        """
+        Forbid deleting templates for non-custom notification types
+        """
+        if not instance.type.is_custom:
+            raise ValidationError(
+                "Deleting templates for non-custom notification types is not allowed."
+            )
+
+        instance.delete()
